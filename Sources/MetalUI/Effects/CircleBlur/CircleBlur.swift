bf1931845d47a1d97babe4e0387b7397639de554
Anton Heestand <anton.heestand@gmail.com>
2023-06-11 12:50:01 +0900

    Adds circle blur effect

--- Sources/MetalUI/Effects/CircleBlur/CircleBlur.swift
+++ Sources/MetalUI/Effects/CircleBlur/CircleBlur.swift
@@ -3,13 +3,18 @@ import SwiftUI
 extension View {
 
     public func circleBlur(_ radius: CGFloat,
+                           sampleCount: Int = 100,
                            isEnabled: Bool = true) -> some View {
-        let function = ShaderFunction(library: .bundle(.module),
-                                      name: "circleBlur")
-        let shader = Shader(function: function, arguments: [
-            .float(radius),
-        ])
-        let maxSampleOffset = CGSize(width: radius, height: radius)
-        return self.layerEffect(shader, maxSampleOffset: maxSampleOffset, isEnabled: isEnabled)
+        SizeReader { size in
+            let function = ShaderFunction(library: .bundle(.module),
+                                          name: "circleBlur")
+            let shader = Shader(function: function, arguments: [
+                .float2(size),
+                .float(radius),
+                .float(CGFloat(sampleCount)),
+            ])
+            let maxSampleOffset = CGSize(width: radius, height: radius)
+            return self.layerEffect(shader, maxSampleOffset: maxSampleOffset, isEnabled: isEnabled)
+        }
     }
 }
