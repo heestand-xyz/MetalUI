95df7cf846ef010c5e3695d294e585e17156e997
Anton Heestand <anton.heestand@gmail.com>
2023-06-15 13:38:56 +0900

    Pixelation

--- Sources/MetalUI/Effects/Pixelate/Pixelate.swift
+++ Sources/MetalUI/Effects/Pixelate/Pixelate.swift
@@ -4,20 +4,19 @@ extension View {
 
     /// Pixelate
     /// - Parameters:
-    ///   - fraction: A higher value will result in more pixelation.
-    ///   A value of 1.0 will result in one pixel, 0.5 in 2x2 and 0.25 in 4x4. The default fraction is 0.1.
-    public func pixelate(fraction: CGFloat = 0.1,
+    ///   - pixelCount: A higher value will result in more pixelation.
+    ///   A value of 2 will result in every 2x2 pixels being averaged into one bigger pixel.
+    ///   The default value of pixelCount is 10.
+    public func pixelate(_ pixelCount: Int = 10,
                          isEnabled: Bool = true) -> some View {
-        SizeReader { size in
-            let function = ShaderFunction(library: .bundle(.module),
-                                          name: "pixelate")
-            let shader = Shader(function: function, arguments: [
-                .boundingRect,
-                .float(fraction),
-            ])
-            let maxSampleOffset = CGSize(width: size.width * fraction,
-                                         height: size.height * fraction)
-            return self.layerEffect(shader, maxSampleOffset: maxSampleOffset, isEnabled: isEnabled)
-        }
+        let function = ShaderFunction(library: .bundle(.module),
+                                      name: "pixelate")
+        let shader = Shader(function: function, arguments: [
+            .float(CGFloat(pixelCount)),
+            .float(CGFloat.pixelsPerPoint),
+        ])
+        let maxSampleOffset = CGSize(width: CGFloat(pixelCount) / .pixelsPerPoint * 2,
+                                     height: CGFloat(pixelCount) / .pixelsPerPoint * 2)
+        return self.layerEffect(shader, maxSampleOffset: maxSampleOffset, isEnabled: isEnabled)
     }
 }
